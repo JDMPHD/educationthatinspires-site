674942c66d55e8471a51d8832705eb923e6f4767
Julian <jdmichelsphd@gmail.com>
2026-02-19 03:46:10 +0800

    fix: restore original working Firebase API key

--- ETI Web/AP-Essay-Workshop/site/js/firebase-config.js
+++ ETI Web/AP-Essay-Workshop/site/js/firebase-config.js
@@ -11,7 +11,7 @@
 // 7. Add your domain to Authentication > Settings > Authorized domains
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaxchF57v8bkXC9q_j6mNX09wJX5lHS1k",
+  apiKey: "AIzaSyC9vDURfexTJ-5pMdYnv8lLhik_zoZ0s1I",
   authDomain: "educationthatinspires-bb11a.firebaseapp.com",
   projectId: "educationthatinspires-bb11a",
   storageBucket: "educationthatinspires-bb11a.firebasestorage.app",
